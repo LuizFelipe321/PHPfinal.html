506aba0f8f9007893edc6189fb73ab1f3ea80be9
Luiz Felipe <102871467+LuizFelipe321@users.noreply.github.com>
2025-11-01 18:59:55 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,15 +1,15 @@
 <?php
-    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-    $servername = "sql309.byetcluster.com";  
-$username = "if0_40241065";  
-$password = "1LjgipstBY";
-$dbname = "if0_40241065_oi";
-    
-    try {
-        $conn = new mysqli($host, $usuario, $senha, $banco);    
-        $conn->set_charset('utfm8mb4');
-    } catch (mysqli_sql_exception $e) {
-      die('Erro ao conectar ao banco de dados.'. $e ->getMessage());
-    }
-    
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+$host = "sql309.infinityfree.com";  // host correto
+$usuario = "if0_40241065";          // seu usuário MySQL
+$senha = "1LjgipstBY";               // sua senha do painel
+$banco = "if0_40241065_oi";          // nome do banco
+
+try {
+    $conn = new mysqli($host, $usuario, $senha, $banco);
+    $conn->set_charset('utf8mb4'); // corrigido
+} catch (mysqli_sql_exception $e) {
+    die('Erro ao conectar ao banco de dados: ' . $e->getMessage());
+}
 ?>
